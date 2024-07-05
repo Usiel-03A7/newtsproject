e835fc849e831ba6ef38c8a0a16fedc85329226f
Sebastian <shebastyan@gmail.com>
2024-07-04 22:38:52 -0600

    make a previews firebase conection

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCBqanYmanj-yq9Tuq0zQGuVkL_c0qhK2c",
+  authDomain: "todolist-c3ad7.firebaseapp.com",
+  projectId: "todolist-c3ad7",
+  storageBucket: "todolist-c3ad7.appspot.com",
+  messagingSenderId: "1060631743901",
+  appId: "1:1060631743901:web:3ec19d2ed6401a9c0e3374"
+};
+
+
+const app = initializeApp(firebaseConfig);
